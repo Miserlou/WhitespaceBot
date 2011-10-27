e8d0e45ebdef2867cfd18ee40d25137c4fabd425
Mathieu Agopian <mathieu@trustive.com>
2011-10-27 17:49:03 +0200

    whitespaces and indentation fixes, two bug fixes

--- robot.py
+++ robot.py
@@ -24,6 +24,7 @@ import json
 #    submit pull req
 #    remove name from list
 
+
 def main():
     parser = argparse.ArgumentParser(description='Whitespace annihilating GitHub robot.\nBy Rich Jones - Gun.io - rich@gun.io')
     parser.add_argument('-u', '--users', help='A text file with usernames.', default='users.txt')
@@ -51,12 +52,12 @@ def main():
         return
 
     repos = 'https://api.github.com/users/' + user + '/repos'
-    r = requests.get (repos, auth = auth) 
+    r = requests.get(repos, auth=auth)
 
     if (r.status_code == 200):
-        resp = simplejson.loads (r.content) 
+        resp = simplejson.loads(r.content)
         topwatch = 0
-        top_repo = '' 
+        top_repo = ''
         for repo in resp:
             if repo['watchers'] > topwatch:
                 top_repo = repo['name']
@@ -89,45 +90,51 @@ def main():
         print "Olding user.."
         old = save_user(old_users_file, user)
 
+
 def save_user(old_users_file, user):
     with open(old_users_file, "a") as id_file:
         id_file.write(user + '\n')
     return True
 
+
 def load_user_list(old_users):
     text_file = open(old_users, "r")
     old = text_file.readlines()
     text_file.close()
-    x=0
+    x = 0
     for hid in old:
         old[x] = hid.rstrip()
-        x=x+1
+        x = x + 1
     return old
+
 
 def get_user(users):
     text_file = open(users, "r")
     u = text_file.readlines()
     text_file.close()
-    return choice(u).rstrip() 
+    return choice(u).rstrip()
+
 
 def fork_repo(user, repo):
     url = 'https://api.github.com/repos/' + user + '/' + repo + '/forks'
     auth = (settings.username, settings.password)
-    r = requests.post (url, auth=auth)
+    r = requests.post(url, auth=auth)
     if (r.status_code == 201):
         resp = simplejson.loads(r.content)
         return resp['ssh_url']
     else:
         return None
 
+
 def clone_repo(clone_url):
     try:
-        args =['/usr/bin/git', 'clone', clone_url] 
-        p = subprocess.Popen(args) 
+        args = ['/usr/bin/git', 'clone', clone_url]
+        p = subprocess.Popen(args)
         p.wait()
         return True
     except Exception, e:
         return False
+
 
 def change_branch(repo):
     #XXX fuck this
@@ -135,48 +142,48 @@ def change_branch(repo):
     repo = os.path.join(settings.pwd, repo)
 
     try:
-        args =['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'branch', 'clean']
+        args = ['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'branch', 'clean']
         p = subprocess.Popen(args)
         p.wait()
-        args =['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'checkout', 'clean']
+        args = ['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'checkout', 'clean']
         p = subprocess.Popen(args)
         p.wait()
         return True
     except Exception, e:
         return False
 
-def fix_repo (repo):
 
+def fix_repo(repo):
     gitdir = os.path.join(settings.pwd, repo, ".git")
     repo = os.path.join(settings.pwd, repo)
     for root, dirs, files in os.walk(repo):
-       for f in files:
-        path = os.path.join(root, f)
+        for f in files:
+            path = os.path.join(root, f)
 
-        # gotta be a way more pythonic way of doing this
-        banned = ['.git', '.py', '.yaml', '.patch', '.hs', '.occ']
-        cont = False 
-        for b in banned:
-            if c in path:
-               cont = True
-        if cont:
-            continue
+            # gotta be a way more pythonic way of doing this
+            banned = ['.git', '.py', '.yaml', '.patch', '.hs', '.occ']
+            cont = False
+            for b in banned:
+                if b in path:
+                    cont = True
+            if cont:
+                continue
 
-        p = subprocess.Popen(['file','-bi',path],stdout=subprocess.PIPE)
+            p = subprocess.Popen(['file', '-bi', path], stdout=subprocess.PIPE)
 
-        while True:
-            o = p.stdout.readline()
-            if o == '':
-                break
-            #XXX: Motherfucking OSX is a super shitty and not real operating system
-            #XXX: and doesn't do file -bi properly
-            if 'text' in o:
-                q = subprocess.Popen(['sed','-i','s/[ \\t]*$//', path])
-                q.wait()
-                args =['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'add', path]
-                pee = subprocess.Popen(args) 
-                pee.wait()
-            if o == '' and p.poll() != None: break
+            while True:
+                o = p.stdout.readline()
+                if o == '':
+                    break
+                #XXX: Motherfucking OSX is a super shitty and not real operating system
+                #XXX: and doesn't do file -bi properly
+                if 'text' in o:
+                    q = subprocess.Popen(['sed', '-i', 's/[ \\t]*$//', path])
+                    q.wait()
+                    args = ['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'add', path]
+                    pee = subprocess.Popen(args)
+                    pee.wait()
+                if o == '' and p.poll() != None: break
 
     git_ignore = os.path.join(repo, '.gitignore')
     if not os.path.exists(git_ignore):
@@ -184,90 +191,96 @@ def fix_repo (repo):
         ignore = '# Compiled source #\n' + \
             '###################\n' + \
             '*.com\n' + \
-            '*.class\n'+ \
-            '*.dll\n'+\
-            '*.exe\n'+\
-            '*.o\n'+\
-            '*.so\n'+\
-            '*.pyc\n\n'+\
-            '# Logs and databases #\n'+\
-            '######################\n'+\
-            '*.log\n\n'+\
-            '# OS generated files #\n'+\
-            '######################\n'+\
-            '.DS_Store*\n'+\
-            'ehthumbs.db\n'+\
-            'Icon?\n'+\
+            '*.class\n' + \
+            '*.dll\n' + \
+            '*.exe\n' + \
+            '*.o\n' + \
+            '*.so\n' + \
+            '*.pyc\n\n' + \
+            '# Logs and databases #\n' + \
+            '######################\n' + \
+            '*.log\n\n' + \
+            '# OS generated files #\n' + \
+            '######################\n' + \
+            '.DS_Store*\n' + \
+            'ehthumbs.db\n' + \
+            'Icon?\n' + \
             'Thumbs.db\n'
         ignorefile.write(ignore)
         ignorefile.close()
         try:
-            args =['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'add', git_ignore] 
-            p = subprocess.Popen(args) 
-            p.wait ()
+            args = ['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'add', git_ignore]
+            p = subprocess.Popen(args)
+            p.wait()
             return True
         except Exception, e:
             return False
 
     return True
 
+
 def commit_repo(repo):
     gitdir = os.path.join(settings.pwd, repo, ".git")
     repo = os.path.join(settings.pwd, repo)
 
     try:
-        message = "Remove whitespace [Gun.io WhitespaceBot]" 
-        args =['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'commit', '-m', message] 
-        p = subprocess.Popen(args) 
+        message = "Remove whitespace [Gun.io WhitespaceBot]"
+        args = ['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'commit', '-m', message]
+        p = subprocess.Popen(args)
         p.wait()
-        return True 
+        return True
     except Exception, e:
         print e
-        return False 
+        return False
+
 
 def push_commit(repo):
     gitdir = os.path.join(settings.pwd, repo, ".git")
     repo = os.path.join(settings.pwd, repo)
     try:
-        args =['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'push', 'origin', 'clean'] 
-        p = subprocess.Popen(args) 
-        p.wait ()
+        args = ['/usr/bin/git', '--git-dir', gitdir, '--work-tree', repo, 'push', 'origin', 'clean']
+        p = subprocess.Popen(args)
+        p.wait()
         return True
     except Exception, e:
         print e
     return False
 
+
 def basic_authorization(user, password):
     s = user + ":" + password
     return "Basic " + s.encode("base64").rstrip()
+
 
 def submit_pull_request(user, repo):
     auth = (settings.username, settings.password)
     url = 'https://api.github.com/repos/' + user + '/' + repo + '/pulls'
     params = {'title': 'Hi! I cleaned up your code for you!', 'body': 'Hi'
             + ' there!\n\nThis is WhitespaceBot. I\'m an [open-source](https://github.com/Gunio/LightWrite) robot that'
-            + ' removes trailing white space in your code, and gives you a gitignore file if you didn\'t have one! '+
+            + ' removes trailing white space in your code, and gives you a gitignore file if you didn\'t have one! ' +
             ' \n\nWhy whitespace? Whitespace is an eyesore for developers who use text editors with dark themes. It\'s not ' +
             ' a huge deal, but it\'s a bit annoying if you use Vim in a terminal. Really, I\'m just a proof of ' +
             ' concept - GitHub\'s V3 API allows robots to automatically improve open source projects, and that\'s really' +
-            ' cool. Hopefully, somebody, maybe you!, will fork me and make me even more useful. My owner is '+
+            ' cool. Hopefully, somebody, maybe you!, will fork me and make me even more useful. My owner is ' +
             '[funding a bounty](http://gun.io/open/12/add-security-flaw-fixing-features-to-whitespacebot) to anybody ' +
             'who can add security fixing features to me. ' +
             '\n\nI\'ve only cleaned your most popular project, and I\'ve added you to a list of users not to contact ' +
             'again, so you won\'t get any more pull requests from me unless you ask. If I\'m misbehaving, please email my ' +
-            'owner and tell him to turn me off! If this is pull request is of no use to you, please just ignore it.\n\n
-            Thanks!\nWhiteSpacebot from [Gun.io](http://gun.io).',
+            'owner and tell him to turn me off! If this is pull request is of no use to you, please just ignore it.\n\n' +
+            'Thanks!\nWhiteSpacebot from [Gun.io](http://gun.io).',
             'base': 'master', 'head': 'GunioRobot:clean'}
 
     req = urllib2.Request(url,
-        headers = {
-            "Authorization": basic_authorization(settings.username, settings.password),
-            "Content-Type": "application/json",
-            "Accept": "*/*",   
-            "User-Agent": "WhitespaceRobot/Gunio", 
-        }, data = json.dumps(params))
+                          headers={
+                              "Authorization": basic_authorization(settings.username, settings.password),
+                              "Content-Type": "application/json",
+                              "Accept": "*/*",
+                              "User-Agent": "WhitespaceRobot/Gunio",
+                          },
+                          data=json.dumps(params))
     f = urllib2.urlopen(req)
     return True
+
 
 def delete_local_repo(repo):
     repo = os.path.join(settings.pwd, repo)
@@ -277,6 +290,6 @@ def delete_local_repo(repo):
     except Exception, e:
         return False
 
+
 if __name__ == '__main__':
         sys.exit(main())
-
